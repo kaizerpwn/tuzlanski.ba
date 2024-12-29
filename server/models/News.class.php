24b12ac15e3ac1d7d0e344f62113b96027507185
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2024-12-29 23:13:02 +0100

    Getters and setters for models

--- server/models/News.class.php
+++ server/models/News.class.php
@@ -12,4 +12,104 @@ class News
     private $keywords;
     private $author;
     private $language;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function getSourceLink()
+    {
+        return $this->source_link;
+    }
+
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    public function getShortDescription()
+    {
+        return $this->short_description;
+    }
+
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    public function getKeywords()
+    {
+        return $this->keywords;
+    }
+
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    public function getLanguage()
+    {
+        return $this->language;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function setSourceLink($source_link)
+    {
+        $this->source_link = $source_link;
+    }
+
+    public function setCategories($categories)
+    {
+        $this->categories = $categories;
+    }
+
+    public function setShortDescription($short_description)
+    {
+        $this->short_description = $short_description;
+    }
+
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    public function setKeywords($keywords)
+    {
+        $this->keywords = $keywords;
+    }
+
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+    }
+
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+    }
 }
--- server/models/User.class.php
+++ server/models/User.class.php
@@ -9,4 +9,74 @@ class User
     private $role;
     private $created_at;
     private $updated_at;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    public function setCreatedAt($created_at)
+    {
+        $this->created_at = $created_at;
+    }
+
+    public function setUpdatedAt($updated_at)
+    {
+        $this->updated_at = $updated_at;
+    }
 }
