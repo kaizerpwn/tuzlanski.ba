43b8a7d647634104fcfbdb6fbc1a30671f327b84
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2025-01-05 19:55:40 +0100

    Admin dashboard UI

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { CategoriesComponent } from './pages/categories/categories.component';
+import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 
 export const routes: Routes = [
   {
@@ -20,5 +21,9 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegisterComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminDashboardComponent,
   },
 ];
