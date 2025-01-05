43ed721279dfee2e5f24b4c45349f555a5f19af7
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2025-01-05 17:24:22 +0100

    Page for rendering only news from one category

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'category/:category',
+    component: CategoriesComponent,
   },
   {
     path: 'login',
--- client/src/app/pages/categories/categories.component.ts
+++ client/src/app/pages/categories/categories.component.ts
@@ -1,12 +1,73 @@
 import { Component } from '@angular/core';
+import { Article } from '../../models/Article';
+import { CATEGORIES } from '../../utils/constants';
+import {
+  findColorForCategory,
+  findMostUsedSubCategories,
+} from '../../utils/helpers';
+import { NewsService } from '../../services/news.service';
+import { ActivatedRoute } from '@angular/router';
+import { BigNewsCardComponent } from '../../components/big-news-card/big-news-card.component';
+import { SmallNewsCardComponent } from '../../components/small-news-card/small-news-card.component';
+import { SideNewsListComponent } from '../../components/side-news-list/side-news-list.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [
+    BigNewsCardComponent,
+    SmallNewsCardComponent,
+    SideNewsListComponent,
+    SidebarComponent,
+  ],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.css'
+  styleUrl: './categories.component.css',
 })
 export class CategoriesComponent {
+  data: Article[] = [];
+  latestArticles: Article[] = [];
+  category: string = '';
 
+  ALL_CATEGORIES = CATEGORIES;
+  findMostUsedSubCategories = findMostUsedSubCategories;
+  findColorForCategory = findColorForCategory;
+
+  constructor(
+    private route: ActivatedRoute,
+    private newsService: NewsService
+  ) {}
+
+  ngOnInit(): void {
+    this.category = this.route.snapshot.paramMap.get('category') || '';
+
+    this.route.params.subscribe((params) => {
+      this.category = params['category'];
+      this.loadLatestArticles();
+    });
+  }
+
+  private loadLatestArticles(): void {
+    this.newsService.getNews(this.category).subscribe((data: any) => {
+      this.latestArticles = data.map(
+        (item: any) =>
+          new Article(
+            item.id,
+            item.title,
+            item.thumbnail,
+            item.image_source,
+            item.images,
+            item.source_link,
+            item.category,
+            item.sub_categories,
+            item.short_description,
+            item.description,
+            item.keywords,
+            item.author,
+            item.language,
+            item.published_at
+          )
+      );
+    });
+  }
 }
