57c2403a8e5868197e93674440914c21429f226a
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2025-01-06 14:40:03 +0100

    Create modal implemented on the news page also

--- client/src/app/pages/admin-news/admin-news.component.ts
+++ client/src/app/pages/admin-news/admin-news.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NewsTableComponent } from '../../components/admin/news-table/news-table.component';
 import { AdminSidebarComponent } from '../../components/admin/admin-sidebar/admin-sidebar.component';
+import { MatDialog } from '@angular/material/dialog';
+import { NewsModalComponent } from '../../components/admin/news-create-modal/news-create-modal.component';
 
 @Component({
   selector: 'app-admin-news',
@@ -9,4 +11,19 @@ import { AdminSidebarComponent } from '../../components/admin/admin-sidebar/admi
   templateUrl: './admin-news.component.html',
   styleUrl: './admin-news.component.css',
 })
-export class AdminNewsComponent {}
+export class AdminNewsComponent {
+  constructor(private dialogRef: MatDialog) {}
+
+  openCreateModal() {
+    const dialogRef = this.dialogRef.open(NewsModalComponent, {
+      width: '800px',
+      disableClose: true,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        console.log(result);
+      }
+    });
+  }
+}
