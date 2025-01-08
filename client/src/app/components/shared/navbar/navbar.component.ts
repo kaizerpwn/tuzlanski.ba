c3439d42feb16bc1ebaa3f97523a626e0db35f18
Ibrahim Okić <70588174+kaizerpwn@users.noreply.github.com>
2025-01-08 17:24:49 +0100

    Responsive design on home page

--- client/src/app/components/shared/navbar/navbar.component.ts
+++ client/src/app/components/shared/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CATEGORIES } from '../../../utils/constants';
 import { RouterLink } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, NgFor],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
@@ -14,4 +15,11 @@ export class NavbarComponent {
     { name: 'Najnovije', color: '#2ad1d2' },
     ...CATEGORIES,
   ];
+
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    console.log('Mobile menu toggled');
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
